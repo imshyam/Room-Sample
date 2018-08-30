9cfb4ccc2f2b1dfa5c84860be0704fb95a26c1d8
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2018-08-31 00:20:08 +0800

    Data Class

---
+++ app/src/main/java/com/shyam/roomsample/CustomData.java
@@ -0,0 +1,19 @@
+package com.shyam.roomsample;
+
+class CustomData {
+    private String id;
+    private String text;
+
+    CustomData(String id, String text) {
+        this.id = id;
+        this.text = text;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
