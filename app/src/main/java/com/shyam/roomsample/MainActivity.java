06192cd1faffabb4d526313610e212e4c1512325
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2018-08-31 00:06:47 +0800

    Add Views

--- app/src/main/java/com/shyam/roomsample/MainActivity.java
+++ app/src/main/java/com/shyam/roomsample/MainActivity.java
@@ -2,12 +2,27 @@ package com.shyam.roomsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button save = findViewById(R.id.button);
+        save.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+                Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
