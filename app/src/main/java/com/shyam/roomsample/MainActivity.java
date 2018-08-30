8db4db7740376a8e49f3e7cd9de938657008885e
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2018-08-31 00:33:50 +0800

    add adapter to activity

--- app/src/main/java/com/shyam/roomsample/MainActivity.java
+++ app/src/main/java/com/shyam/roomsample/MainActivity.java
@@ -2,11 +2,21 @@ package com.shyam.roomsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    int textId = 0;
+    EditText editText;
+    CustomViewAdapter customViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +24,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         Button save = findViewById(R.id.button);
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        editText = findViewById(R.id.editText);
+
         save.setOnClickListener(this);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+
+        List<CustomData> customDataList = new ArrayList<>();
+        customViewAdapter = new CustomViewAdapter(customDataList);
+        recyclerView.setAdapter(customViewAdapter);
 
     }
 
@@ -22,7 +40,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button:
-                Toast.makeText(getApplicationContext(), "Button Clicked", Toast.LENGTH_SHORT).show();
+                String text = editText.getText().toString();
+                String id = String.valueOf(textId);
+                CustomData data = new CustomData(id, text);
+                customViewAdapter.addItem(data);
+                textId++;
         }
     }
 }
