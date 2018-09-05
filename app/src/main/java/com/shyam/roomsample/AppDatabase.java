20a3dfb4bea7c066402f2fbdb0dd7b42af4704fd
Shyam Sundar Choudhary <sschoudhary0001@gmail.com>
2018-09-06 01:08:21 +0800

    Create DB instance

--- app/src/main/java/com/shyam/roomsample/AppDatabase.java
+++ app/src/main/java/com/shyam/roomsample/AppDatabase.java
@@ -1,9 +1,20 @@
 package com.shyam.roomsample;
 
 import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
 
 @Database(entities = CustomData.class, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract CustomDataDao customDataDao();
+
+    private static AppDatabase INSTANCE;
+
+    public static AppDatabase getDatabase(Context context) {
+        if(INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context, AppDatabase.class, "my-db").build();
+        }
+        return INSTANCE;
+    }
 }
